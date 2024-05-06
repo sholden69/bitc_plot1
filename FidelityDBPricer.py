5e488db59c364a10efa8723a7ffc8fdf0d2fd881
Simon Holden <simon.holden@gmail.com>
2024-05-06 10:27:30 +0100

    Fixed 404 issue

--- FidelityDBPricer.py
+++ FidelityDBPricer.py
@@ -7,6 +7,7 @@ import requests
 from bs4 import BeautifulSoup
 import numpy as np
 import re
+from sqlalchemy.types import Integer, Text, String, DateTime, VARCHAR, Float
 
 def get_price(url_core):
     url='https://www.fidelity.co.uk/factsheet-data/factsheet/'+url_core+'/key-statistics'
@@ -14,7 +15,8 @@ def get_price(url_core):
     page=requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     res = soup.find(class_='detail_value')
-    
+    if res==None:  #missing price or a 404
+        return -1 
     for r in res:
         r=r.replace(',', '')
         if (r[-1]=='p'):
@@ -27,28 +29,32 @@ def get_price(url_core):
                 prc=float(r[1:])
             except ValueError:
                 prc=-1
+    print("price",prc)
     return prc  
 
-# https://docs.sqlalchemy.org/en/14/core/engines.html#postgresql 
-# dialect+driver://username:password@host:port/database'
-from sqlalchemy.types import Integer, Text, String, DateTime, VARCHAR, Float
-sqlEngine=create_engine('mysql+pymysql://guest:guest@127.0.0.1/finance', pool_recycle=3600,echo=True)
-dbConnection=sqlEngine.connect()
-dbConnection.execute("TRUNCATE TABLE fidelity_latest_prices")
-df=pd.read_sql("SELECT urlcore,fundlabel FROM fidelity_info WHERE urlcore IS NOT null", dbConnection)
-df['price'] = df['urlcore'].apply(get_price)
-# drop any missing fidelity_prices
-df=df[df['price'].ne(-1)]
-# write back to the db
-df.to_sql(
-    'fidelity_latest_prices',
-    sqlEngine,
-    if_exists='append',
-    index=False,
-    chunksize=500,
-    dtype={
-        "urlcore": VARCHAR(100),
-        "fundlabel": VARCHAR(50),
-        "price" : Float
-    }
-)
+def main():
+    # https://docs.sqlalchemy.org/en/14/core/engines.html#postgresql 
+    # dialect+driver://username:password@host:port/database'
+    sqlEngine=create_engine('mysql+pymysql://guest:guest@127.0.0.1/finance', pool_recycle=3600,echo=True)
+    dbConnection=sqlEngine.connect()
+    dbConnection.execute("TRUNCATE TABLE fidelity_latest_prices")
+    df=pd.read_sql("SELECT urlcore,fundlabel FROM fidelity_info WHERE urlcore IS NOT null", dbConnection)
+    df['price'] = df['urlcore'].apply(get_price)
+    # drop any missing fidelity_prices
+    df=df[df['price'].ne(-1)]
+    # write back to the db
+    df.to_sql(
+        'fidelity_latest_prices',
+        sqlEngine,
+        if_exists='append',
+        index=False,
+        chunksize=500,
+        dtype={
+            "urlcore": VARCHAR(100),
+            "fundlabel": VARCHAR(50),
+            "price" : Float
+        }
+    )
+
+if __name__ == "__main__":
+    main()
